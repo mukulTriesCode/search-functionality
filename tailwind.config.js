3cf8943b0e66de2e461a962d4f658f1fab850fe2
Mukul Sharma <mukulsharma262002@gmail.com>
2024-10-16 18:17:46 +0530

    Setup: Tailwind config and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#252422",
+        light: "#fffcf2",
+        primary: "#EB5E28",
+      },
+      fontFamily: {
+        basheq: "var(--var-basheq)",
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+      },
+    },
   },
   plugins: [],
-}
+};
