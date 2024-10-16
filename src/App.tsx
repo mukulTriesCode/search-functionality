6ae56f89804fdc2ce24f48faf8fc116313429f9d
Mukul Sharma <mukulsharma262002@gmail.com>
2024-10-16 17:33:10 +0530

    Feature: Added home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index path="/" element={<>home</>} />
+        <Route index path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
