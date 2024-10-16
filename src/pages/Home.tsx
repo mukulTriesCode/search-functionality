80962e91cda852ca721efb2f76c0c950b2018f7c
Mukul Sharma <mukulsharma262002@gmail.com>
2024-10-16 18:18:00 +0530

    Feature: Added homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Home: React.FC = () => {
-  return <div>Home</div>;
+  return (
+    <main className="min-h-screen relative font-basheq">
+      <div className="min-h-screen w-full bg-dark absolute left-0 top-0 -z-10">
+        <div
+          className="min-h-screen w-full opacity-60"
+          style={{ backgroundImage: "url('./noise.svg')" }}
+        ></div>
+      </div>
+      <section className="container text-light py-20 text-center">
+        <h1 className="text-8xl">
+          Enter the{" "}
+          <span className="underline decoration-primary underline-offset-[1rem] decoration-4">
+            realm
+          </span>
+        </h1>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
