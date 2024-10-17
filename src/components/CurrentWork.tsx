6291cc1e0ffc63d10fca883e1981f55f41263570
Mukul Sharma <mukulsharma262002@gmail.com>
2024-10-17 18:41:43 +0530

    Feature: Updated data and styling

--- src/components/CurrentWork.tsx
+++ src/components/CurrentWork.tsx
@@ -4,7 +4,8 @@ const CurrentWork: React.FC = () => {
   const data = [
     {
       title: "Currently",
-      description: "I’m a <span>Frontend</span> Developer at RW Infotech, an Headless CMS experts company.",
+      description:
+        "I’m a <span>Frontend</span> Developer at RW Infotech, an Headless CMS experts company.",
     },
     {
       title: "Availability",
@@ -21,24 +22,39 @@ const CurrentWork: React.FC = () => {
   ];
 
   return (
-    <section className="container my-20 text-light flex flex-col gap-9">
-      {data.map((item, index) => (
-        <div key={index}>
-          <h2 className="current-work-heading mb-4">{item.title}</h2>
-          {item.description && (
-            <p className="font-fraunces font-light text-[1.75rem] [&_span]:text-primary" dangerouslySetInnerHTML={{ __html: item.description }}></p>
-          )}
-          {item.links && (
-            <div className="flex flex-col gap-2">
-              {item.links.map((link, linkIndex) => (
-                <a href={link.url} key={linkIndex} className="font-fraunces text-primary hover:underline underline-offset-4 decoration-2 duration-300 text-[1.75rem]">
-                  {link.name}
-                </a>
-              ))}
-            </div>
-          )}
-        </div>
-      ))}
+    <section className="container my-20 text-light flex">
+      <div className="flex flex-col gap-9 w-3/5">
+        {data.map((item, index) => (
+          <div key={index}>
+            <h2 className="current-work-heading mb-4">{item.title}</h2>
+            {item.description && (
+              <p
+                className="font-fraunces font-light text-[1.75rem] [&_span]:text-primary"
+                dangerouslySetInnerHTML={{ __html: item.description }}
+              ></p>
+            )}
+            {item.links && (
+              <div className="flex flex-col gap-2">
+                {item.links.map((link, linkIndex) => (
+                  <a
+                    href={link.url}
+                    key={linkIndex}
+                    className="font-fraunces text-primary hover:underline underline-offset-4 decoration-2 duration-300 text-[1.75rem]"
+                  >
+                    {link.name}
+                  </a>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+      <div className="w-2/5 h-fit top-10 sticky">
+        <img
+          src="https://cdn.dribbble.com/users/776205/screenshots/3857304/media/ebcb3befb35eeb78c0466d06ff244d25.gif"
+          alt=""
+        />
+      </div>
     </section>
   );
 };
