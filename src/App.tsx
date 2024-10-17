38e23c548b528dcfcc0dd4e8c39db5d66487820c
Mukul Sharma <mukulsharma262002@gmail.com>
2024-10-17 12:29:28 +0530

    Feature: Added animated cursor in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   return (
@@ -10,6 +11,27 @@ function App() {
           <Route index path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
+      <AnimatedCursor
+        innerSize={16}
+        outerSize={32}
+        color="235, 94, 40"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={1.5}
+        clickables={[
+          "a",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          "label[for]",
+          "select",
+          "textarea",
+          "button",
+          ".link",
+        ]}
+      />
     </main>
   );
 }
