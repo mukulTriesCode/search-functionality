ecff1118ebfae13a0f6c75f3a98e66dbd05ac2da
Mukul Sharma <mukulsharma262002@gmail.com>
2024-10-17 16:02:25 +0530

    Updated cursor event in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 const Header: React.FC = () => {
   return (
     <header className="container py-5 border-b text-3xl flex justify-between text-light font-fraunces">
-      <h5>badjax</h5>
+      <h5>
+        <Link className="cursor-none" to="/">badjax</Link>
+      </h5>
       <div className="flex gap-5">
-        <Link to="/work">work</Link>
-        <Link to="/music">music</Link>
+        <Link className="cursor-none" to="/work">work</Link>
+        <Link className="cursor-none" to="/music">music</Link>
       </div>
     </header>
   );
