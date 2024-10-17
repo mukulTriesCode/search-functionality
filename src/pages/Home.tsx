dd88f40a5923cab314d83ec9b38f7fa85d60b83d
Mukul Sharma <mukulsharma262002@gmail.com>
2024-10-17 11:56:42 +0530

    Feature: Created homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,24 @@
 import React from "react";
+import Heading from "../components/Heading";
+import BannerImage from "../components/BannerImage";
+import Work from "../components/Work";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 const Home: React.FC = () => {
   return (
-    <main className="min-h-screen relative font-basheq">
-      <div className="min-h-screen w-full bg-dark absolute left-0 top-0 -z-10">
+    <main className="min-h-screen h-full relative">
+      <div className="min-h-screen w-full h-full bg-dark absolute left-0 top-0 -z-10">
         <div
-          className="min-h-screen w-full opacity-60"
+          className="min-h-screen h-full w-full opacity-60"
           style={{ backgroundImage: "url('./noise.svg')" }}
         ></div>
       </div>
-      <section className="container text-light py-20 text-center">
-        <h1 className="text-8xl">
-          Enter the{" "}
-          <span className="underline decoration-primary underline-offset-[1rem] decoration-4">
-            realm
-          </span>
-        </h1>
-      </section>
+      <Header />
+      <Heading />
+      <BannerImage />
+      <Work />
+      <Footer />
     </main>
   );
 };
