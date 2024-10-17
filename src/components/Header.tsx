afdecae207002d02531f6558b16635f9cf5d82ea
Mukul Sharma <mukulsharma262002@gmail.com>
2024-10-17 15:41:41 +0530

    Feature: Added nav links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
-  return <header className="container py-5 border-b-2 text-3xl text-end text-light font-fraunces">Mukul Sharma</header>;
+  return (
+    <header className="container py-5 border-b text-3xl flex justify-between text-light font-fraunces">
+      <h5>badjax</h5>
+      <div className="flex gap-5">
+        <Link to="/work">work</Link>
+        <Link to="/music">music</Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
