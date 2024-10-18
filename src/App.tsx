0bad432453870f86a00952ba6df67d49ddce75c2
Mukul Sharma <mukulsharma262002@gmail.com>
2024-10-18 11:50:29 +0530

    Feature: Added layout component in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,24 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import AnimatedCursor from "react-animated-cursor";
-import useMobile from "./hooks/useMobile";
 import Music from "./pages/Music";
 import WorkPage from "./pages/Work";
+import Layout from "./components/Layout";
 
 function App() {
-  const isMobile = useMobile();
   return (
-    <main className="font-basheq">
+    <>
       <BrowserRouter>
         <Routes>
-          <Route index path="/" element={<Home />} />
-          <Route path="/work" element={<WorkPage />} />
-          <Route path="/music" element={<Music />} />
-          <Route path="*" element={<>404</>} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/work" element={<WorkPage />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="*" element={<>404</>} />
+          </Route>
         </Routes>
       </BrowserRouter>
-      {!isMobile && (
-        <AnimatedCursor
-          innerSize={16}
-          outerSize={32}
-          color="235, 94, 40"
-          outerAlpha={0.2}
-          innerScale={0.7}
-          outerScale={1.5}
-          clickables={[
-            "a",
-            'input[type="text"]',
-            'input[type="email"]',
-            'input[type="number"]',
-            'input[type="submit"]',
-            'input[type="image"]',
-            "label[for]",
-            "select",
-            "textarea",
-            "button",
-            ".link",
-            "summary",
-          ]}
-        />
-      )}
-    </main>
+    </>
   );
 }
 
