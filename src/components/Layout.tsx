f53723a1225b49e870a76a7b48fcada7bc57e78f
Mukul Sharma <mukulsharma262002@gmail.com>
2024-10-18 15:17:22 +0530

    Feature: Added meta fields in layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,47 +4,54 @@ import AnimatedCursor from "react-animated-cursor";
 import useMobile from "../hooks/useMobile";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
-import { headerData } from "../helpers/data";
+import { headerData, metaFields } from "../helpers/data";
 
 const Layout: React.FC = () => {
   const isMobile = useMobile();
 
   return (
-    <main className="font-basheq min-h-screen h-full relative text-light">
-      <div className="min-h-screen w-full h-full bg-dark absolute left-0 top-0 -z-10">
-        <div
-          className="min-h-screen h-full w-full opacity-60"
-          style={{ backgroundImage: "url('./noise.svg')" }}
-        ></div>
-      </div>
-      <Header headerData={headerData} />
-      <Outlet />
-      {!isMobile && (
-        <AnimatedCursor
-          innerSize={16}
-          outerSize={32}
-          color="235, 94, 40"
-          outerAlpha={0.2}
-          innerScale={0.7}
-          outerScale={1.5}
-          clickables={[
-            "a",
-            'input[type="text"]',
-            'input[type="email"]',
-            'input[type="number"]',
-            'input[type="submit"]',
-            'input[type="image"]',
-            "label[for]",
-            "select",
-            "textarea",
-            "button",
-            ".link",
-            "summary",
-          ]}
-        />
-      )}
-      <Footer />
-    </main>
+    <>
+      <head>
+        {metaFields.map((field, index) => (
+          <meta key={index} name={field.name} content={field.content} />
+        ))}
+      </head>
+      <main className="font-basheq min-h-screen h-full relative text-light">
+        <div className="min-h-screen w-full h-full bg-dark absolute left-0 top-0 -z-10">
+          <div
+            className="min-h-screen h-full w-full opacity-60"
+            style={{ backgroundImage: "url('./noise.svg')" }}
+          ></div>
+        </div>
+        <Header headerData={headerData} />
+        <Outlet />
+        {!isMobile && (
+          <AnimatedCursor
+            innerSize={16}
+            outerSize={32}
+            color="235, 94, 40"
+            outerAlpha={0.2}
+            innerScale={0.7}
+            outerScale={1.5}
+            clickables={[
+              "a",
+              'input[type="text"]',
+              'input[type="email"]',
+              'input[type="number"]',
+              'input[type="submit"]',
+              'input[type="image"]',
+              "label[for]",
+              "select",
+              "textarea",
+              "button",
+              ".link",
+              "summary",
+            ]}
+          />
+        )}
+        <Footer />
+      </main>
+    </>
   );
 };
 
