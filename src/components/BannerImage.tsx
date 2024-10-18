4bd270aae8b4c064ea2b2d080cba1252b94c11e3
Mukul Sharma <mukulsharma262002@gmail.com>
2024-10-18 11:53:33 +0530

    Feature: Added props in banner Image

--- src/components/BannerImage.tsx
+++ src/components/BannerImage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const BannerImage: React.FC = () => {
+const BannerImage: React.FC<{src: string}> = ({src}) => {
   return (
     <section className="">
       <div className="w-full h-[400px] object-cover relative">
@@ -14,7 +14,7 @@ const BannerImage: React.FC = () => {
         </div>
         <img
           className="w-full h-full object-cover grayscale"
-          src="https://images.unsplash.com/photo-1598559411608-e6dddb68e5f4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src={src}
           alt="banner-image"
           draggable="false"
         />
