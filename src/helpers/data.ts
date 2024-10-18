59635ae4bb7923be73fbf0fa8295c9a7dc8c2fd0
Mukul Sharma <mukulsharma262002@gmail.com>
2024-10-18 13:29:13 +0530

    Feature: Added a data file

---
+++ src/helpers/data.ts
@@ -0,0 +1,110 @@
+export const headerData = {
+  title: "Mukul Sharma",
+  navLinks: [
+    {
+      label: "work",
+      url: "/work",
+    },
+    {
+      label: "music",
+      url: "/music",
+    },
+  ],
+};
+
+export const homepage = {
+  title: "Enter the <u>Realm</u><br />Of Frontend",
+  bannerImage:
+    "https://images.unsplash.com/photo-1598559411608-e6dddb68e5f4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+};
+
+export const workPage = {
+  title: "<u>Skills</u> and<br />Work",
+  bannerImage:
+    "https://images.unsplash.com/photo-1599680756838-307f6d3c4ada?q=80&w=1940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+};
+
+export const currentWorkData = [
+  {
+    title: "Currently",
+    description:
+      "I’m a <span>Frontend</span> Developer at RW Infotech, an Headless CMS experts company.",
+  },
+  {
+    title: "Availability",
+    description: "I’m currently available to work togather.",
+  },
+  {
+    title: "Links",
+    links: [
+      { name: "Github", url: "#" },
+      { name: "Twitter", url: "#" },
+      { name: "LinkedIn", url: "#" },
+    ],
+  },
+];
+
+export const workData = {
+  title: "Selected <u>Work</u>",
+  projects: [
+    {
+      title: "React Code Editor",
+      description:
+        "Code editor is created using React + TS with Redux Toolkit for state management",
+    },
+    {
+      title: "E-commerce Website",
+      description:
+        "E-commerce website built using React, Redux, and Firebase for authentication and database management",
+    },
+    {
+      title: "Portfolio Website",
+      description:
+        "Portfolio website created using React, Redux, and CSS for styling and animations",
+    },
+    {
+      title: "Chatbot Application",
+      description:
+        "Chatbot application developed using React, Redux, and Dialogflow for conversational AI",
+    },
+  ],
+};
+
+export const skillData = [
+  {
+    label: "Skills",
+    data: [
+      {
+        label: "Frontend",
+        technologies: [
+          { label: "HTML", logo: "" },
+          { label: "CSS", logo: "" },
+          { label: "SASS", logo: "" },
+          { label: "BootStrap", logo: "" },
+          { label: "Tailwind CSS", logo: "" },
+          { label: "JavaScript", logo: "" },
+          { label: "TypeScript", logo: "" },
+          { label: "ReactJS", logo: "" },
+          { label: "NextJS", logo: "" },
+          { label: "Redux", logo: "" },
+        ],
+      },
+      {
+        label: "Headless CMS",
+        technologies: [
+          { label: "Prismic", logo: "" },
+          { label: "Sanity", logo: "" },
+        ],
+      },
+      {
+        label: "API",
+        technologies: [
+          { label: "Axios", logo: "" },
+          { label: "GraphQL", logo: "" },
+          { label: "Postman", logo: "" },
+          { label: "Insomnia", logo: "" },
+        ],
+      },
+    ],
+  },
+];
